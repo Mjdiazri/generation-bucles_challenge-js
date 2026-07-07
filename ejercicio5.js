f4bd5390e701325bf0145e10995fd928ed64a961
Mjdiazri <citruss2012@hotmail.es>
2026-07-07 11:33:53 -0500

    Se ajusta forma en la presentacion de los datos

--- ejercicio5.js
+++ ejercicio5.js
@@ -1,5 +1,5 @@
     console.log("\n********* EJERCICIO 5 *********\n\n");
-let users = [{usuario: "Juana", movimientos:[645, -450, 1345, -632, -200, 150]},{usuario: "Carlos", movimientos:[64500, 450000, 1345000, -63200, -200000, 150000]}, {usuario: "Diego", movimientos:[64500, 450000, 1345000, 63200, -200000, 150000]}, {usuario: "Maria", movimientos:[64500, 450000, 1345000, 63200, 200000, 150000]}, ];
+let users = [{usuario: "Juana", movimientos:[645, -450, 1345, -632, -200, 150]},{usuario: "Carlos", movimientos:[450, 500, 3450, -620, -200, 500]}, {usuario: "Diego", movimientos:[560, 550, 5134, 2632, -20, 515]}, {usuario: "Maria", movimientos:[645, 450, 1345, 632, 20, 150]}, ];
 
 for(let i = 0; i < users.length; i++){
     let totalUsuarios = 0;
